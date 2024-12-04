f86af7c2455a12d344d383b7c0a8d0ccc5fd4dff
sjorge <sjorge@blackdot.be>
2024-12-04 13:31:31 +0100

    feat: lib/anime/anidb - implement title search

--- src/lib/anime/anidb.ts
+++ src/lib/anime/anidb.ts
@@ -8,23 +8,24 @@ import toml from "@iarna/toml";
 import axios from "axios";
 import { convert as convertXmlDoc } from "xmlbuilder2";
 import { deepmergeInto } from "deepmerge-ts";
+import levenshtein from "fast-levenshtein";
 
 import type { Config } from "lib/config";
 import type { AnimeId, AnimeTitleVariant } from "lib/anime";
 
-const TitleURL = "https://anidb.net/api/anime-titles.xml.gz";
+const TitlesDBUrl = "https://anidb.net/api/anime-titles.xml.gz";
 
-type TitleXMLAnimeTitle = {
-        "@type": string;
-        "@xml:lang": string;
-        "#": string;
+type TitlesDBTitleObject = {
+  "@type": string;
+  "@xml:lang": string;
+  "#": string;
 };
 
-type TitleXML = {
+type TitlesDBObject = {
   animetitles: {
     anime: {
       "@aid": number;
-      title: TitleXMLAnimeTitle | TitleXMLAnimeTitle[];
+      title: TitlesDBTitleObject | TitlesDBTitleObject[];
     }[];
   };
 };
@@ -33,10 +34,13 @@ type TitleMapping = {
   [anidb: number]: AnimeTitleVariant[];
 };
 
+const TitleAidRegEx = new RegExp(/\[anidb-(?<aid>\d+)\]/);
+
 export class AniDBResolver {
   private titleFile: string;
   private titleCacheAge: number;
   private titleCache: TitleMapping = {};
+  private fuzzyMatchThreshhold: number = 3;
 
   public constructor(config: Config) {
     this.titleCacheAge = config.cache.anidb_age;
@@ -66,22 +70,24 @@ export class AniDBResolver {
     // load from remote
     this.titleCache = {};
 
-    const titles = await axios.get(TitleURL, { responseType: "arraybuffer" });
+    const titles = await axios.get(TitlesDBUrl, {
+      responseType: "arraybuffer",
+    });
     const titlesXml = convertXmlDoc(zlib.gunzipSync(titles.data).toString(), {
       format: "object",
-    }) as unknown as TitleXML;
+    }) as unknown as TitlesDBObject;
 
     for (const entry of titlesXml.animetitles.anime) {
       const aid: number = entry["@aid"];
 
       if (this.titleCache[aid] === undefined) this.titleCache[aid] = [];
 
-      // ensure single TitleXMLAnimeTitle is mapped as TitleXMLAnimeTitle[]
-      if ((entry.title as TitleXMLAnimeTitle)["#"]) {
-        entry.title = [entry.title] as TitleXMLAnimeTitle[];
+      // ensure single TitlesDBTitleObject is mapped as TitlesDBTitleObject[]
+      if ((entry.title as TitlesDBTitleObject)["#"]) {
+        entry.title = [entry.title] as TitlesDBTitleObject[];
       }
 
-      for (const entryTitle of entry.title as TitleXMLAnimeTitle[]) {
+      for (const entryTitle of entry.title as TitlesDBTitleObject[]) {
         const title: AnimeTitleVariant = {
           title: entryTitle["#"],
           type: entryTitle["@type"],
@@ -107,6 +113,55 @@ export class AniDBResolver {
       encoding: "utf8",
     });
     fs.chmodSync(this.titleFile, 0o660);
+  }
+
+  public titleFromId(id: AnimeId): AnimeTitleVariant[] | undefined {
+    return this.titleCache[id.anidb];
+  }
+
+  public resolveFromTitle(title: string): AnimeId | undefined {
+    // match [anidb-<aid>] tag
+    const aidMatch: RegExpExecArray | null = TitleAidRegEx.exec(title);
+    if (aidMatch !== null) {
+      return { anidb: parseInt(aidMatch[1]) } as AnimeId;
+    }
+
+    // fuzzy search
+    const titleNormalized: string = title.replace("⁄", "/");
+
+    let exact_match: number | undefined;
+    let best_match: number | undefined;
+    let best_match_score: number = 0;
+
+    Object.entries(this.titleCache).forEach(([key, value]) => {
+      const aid: number = parseInt(key);
+      const titles: AnimeTitleVariant[] = value;
+      titles.forEach((variant: AnimeTitleVariant) => {
+        if (variant.title == titleNormalized) {
+          exact_match = aid;
+        } else {
+          const distance: number = levenshtein.get(
+            titleNormalized,
+            variant.title,
+            { useCollator: true },
+          );
+          if (distance <= this.fuzzyMatchThreshhold) {
+            if (best_match == undefined || best_match_score > distance) {
+              best_match = aid;
+              best_match_score = distance;
+            }
+          }
+        }
+      });
+    });
+
+    if (exact_match !== undefined) {
+      return { anidb: exact_match } as AnimeId;
+    } else if (best_match !== undefined) {
+      return { anidb: best_match } as AnimeId;
+    }
+
+    return undefined;
   }
 }
 
