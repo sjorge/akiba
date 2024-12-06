f1a82fc9e065e02a2623066024a0d792f1663380
sjorge <sjorge@blackdot.be>
2024-12-06 12:28:24 +0100

    feat: cmd/rename - support for --print-ed2klinks

--- src/cmd/rename.ts
+++ src/cmd/rename.ts
@@ -1,6 +1,7 @@
 import type { OptionValues } from "@commander-js/extra-typings";
 import {
   Command,
+  Argument,
   Option,
   InvalidArgumentError,
 } from "@commander-js/extra-typings";
@@ -16,6 +17,9 @@ import {
   animeFormatValidate,
   AnimeFormatStringException,
 } from "lib/anime/renamer";
+
+const ignoreExt: string[] = [".nfo", ".jpg", ".png"];
+const ignoreFile: string[] = [".plexmatch", ".ignore"];
 
 /*
  * Entrypoint `rename` action for commander-js
@@ -44,11 +48,60 @@ export async function renameAction(
   );
 
   // identify episode file(s)
-  // XXX: handle dir or single file
-  if (!fs.existsSync(animePath) || !fs.statSync(animePath).isDirectory()) {
-    log(`Directory "${animePath}" does not exist!`, "error");
+  if (!fs.existsSync(animePath)) {
+    log(`Directory or file "${animePath}" does not exist!`, "error");
     process.exitCode = 1;
     return;
+  }
+
+  const episodeFiles: string[] = [];
+  if (fs.statSync(animePath).isDirectory()) {
+    for (const episodeFile of fs.readdirSync(animePath)) {
+      const episodePath = path.join(animePath, episodeFile);
+
+      // ignore certain files and extension
+      if (ignoreFile.includes(episodeFile)) continue;
+      if (ignoreExt.includes(path.extname(episodeFile))) continue;
+
+      // ignore symlinks and directories
+      const episodeFsStats = fs.lstatSync(episodePath);
+      if (episodeFsStats.isSymbolicLink() || !episodeFsStats.isFile()) continue;
+
+      episodeFiles.push(episodePath);
+    }
+  } else {
+    episodeFiles.push(animePath);
+  }
+
+  const hashOnly = opts.printEd2klinks as boolean;
+  const targetPath = opts.targetPath
+    ? path.resolve(`${opts.targetPath}`)
+    : config.renamer.targetPath || path.resolve(".");
+  const format = opts.format ? `${opts.format}` : config.renamer.format;
+
+  if (hashOnly) {
+    log("Printing ed2khash links ...");
+  } else {
+    log(`Target Path: ${targetPath}`);
+    log(`Format: ${format}`);
+    log("Renaming files ...");
+  }
+
+  for (const episodeFile of episodeFiles) {
+    if (!hashOnly)
+      log(`${path.basename(episodeFile)}: Identifying ...`, "step");
+
+    const episode = await animeRenamer.identify(episodeFile, hashOnly);
+
+    if (hashOnly) {
+      console.log(episode.ed2khash.link); // use console.log for raw output
+    } else if (episode.data) {
+      log(`${path.basename(episodeFile)}: Identifying ...`, "done");
+      // XXX: animeRenamer.rename(episodeData, format, targetPath);
+    } else {
+      log(`${path.basename(episodeFile)}: Identifying ...`, "error");
+      process.exitCode = 1;
+    }
   }
 }
 
@@ -59,7 +112,15 @@ export function addRenameCommand(program: Command): void {
   program
     .command("rename")
     .description("rename episodes")
-    .argument("<path>", "path to anime show directory or episode file")
+    .addArgument(
+      new Argument(
+        "<path>",
+        "path to anime show directory or episode file",
+      ).argParser((value: string) => {
+        if (!path.isAbsolute(value)) return path.resolve(value);
+        return value;
+      }),
+    )
     .addOption(
       new Option("--rehash", "force rehash of already hashed").default(false),
     )
@@ -88,6 +149,24 @@ export function addRenameCommand(program: Command): void {
         }
         return value;
       }),
+    )
+    .addOption(
+      new Option("--target-path <path>", "overwrite desination path").argParser(
+        (value: string) => {
+          if (!path.isAbsolute(value)) value = path.resolve(value);
+
+          if (!fs.existsSync(value) || !fs.lstatSync(value).isDirectory()) {
+            throw new InvalidArgumentError(
+              `Path ${value} is no a directory or does not exist!`,
+            );
+          }
+
+          return value;
+        },
+      ),
+    )
+    .addOption(
+      new Option("--print-ed2klinks", "only print ed2klinks").default(false),
     )
     .action(renameAction);
 }
