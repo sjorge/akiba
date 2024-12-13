693829ccab19f4150322b289dfd6f6d18b1215f0
sjorge <sjorge@blackdot.be>
2024-12-13 11:23:32 +0100

    feat: prepare for v2.1.2

--- src/vars.ts
+++ src/vars.ts
@@ -1,4 +1,4 @@
 export const _DEFINE_PROG: string = "akiba";
-export const _DEFINE_VER: string = "2.1.1";
+export const _DEFINE_VER: string = "2.1.2";
 
 // vim: tabstop=2 shiftwidth=2 softtabstop=0 smarttab expandtab
